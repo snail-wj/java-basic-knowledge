1411687ff29efbff519258f79982b88395f2e744
wj <2927592708@qq.com>
2018-08-13 07:34:36 +0800

    修改cyclicBarrier

--- src/main/java/com/example/thread/cyclicBarrier/Race.java
+++ src/main/java/com/example/thread/cyclicBarrier/Race.java
@@ -8,11 +8,14 @@ import java.util.concurrent.Executors;
 /**
  * @author WJ
  * @date 2018/8/12
+ * CyclicBarrier：当计数达到指定的值时，计数置为0，重新开始，
+ * 所以文中一下代码是3个，并且线程一定要是3的倍数，否则，会阻塞一部分线程的执行
  */
 public class Race {
-    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(8);
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
 
     private static void start(){
+        System.out.println(cyclicBarrier.getNumberWaiting());
         ArrayList<Athlete> athletes = new ArrayList<>();
         athletes.add(new Athlete(cyclicBarrier, "博尔特"));
         athletes.add(new Athlete(cyclicBarrier, "鲍威尔"));
@@ -22,7 +25,8 @@ public class Race {
         athletes.add(new Athlete(cyclicBarrier, "苏炳添"));
         athletes.add(new Athlete(cyclicBarrier, "路人甲"));
         athletes.add(new Athlete(cyclicBarrier, "路人乙"));
-        ExecutorService executor = Executors.newFixedThreadPool(8);
+        athletes.add(new Athlete(cyclicBarrier, "路人丙"));
+        ExecutorService executor = Executors.newFixedThreadPool(3);
         for (Athlete athlete : athletes){
             executor.execute(athlete);
         }
